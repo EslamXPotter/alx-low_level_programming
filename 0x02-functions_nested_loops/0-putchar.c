af7af1d87e42fdee730f0bf893c733b0b1925596
EslamXPotter <xroseloverx@gmail.com>
2023-03-21 14:25:09 -0700

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <unustd.h>
+#include <unistd.h>
 /**
  * _putchar - writes the character c tostdout
  * @c: The character to print
@@ -9,5 +9,5 @@
  */
 int _putchar(char c)
 {
-	return (write(1, &c, 1))
+	return (write(1, &c, 1));
 }
