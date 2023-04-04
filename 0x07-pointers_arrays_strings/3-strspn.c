14c0d3ac424f484f86470af6c793f1123ea85808
EslamXPotter <xroseloverx@gmail.com>
2023-04-03 17:22:17 -0700

    C - more pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strspn - function that gets the lenght of a prefix sunstring
+ * @s: input
+ * @accept: input
+ * Return: number of bytes
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int n = 0;
+	int x;
+
+	while (*s)
+	{
+		for (x = 0; accept[x]; x++)
+		{
+			if (*s == accept[x])
+			{
+				n++;
+				break;
+			}
+			else if (accept[x + 1] == '\0')
+				return (n);
+		}
+		s++;
+	}
+	return (n);
+}
